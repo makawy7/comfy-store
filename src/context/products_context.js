3ee442a3ce8d033493cddaed2bcfecf5f27f0daf
abdallah <abdallahelmekawy6@gmail.com>
2023-03-29 01:52:50 +0200

    global action types constants

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -1,5 +1,6 @@
 import { createContext, useContext, useReducer } from "react";
 import reducer from "../reducers/products_reducer";
+import { SIDEBAR_OPEN, SIDEBAR_CLOSE } from "../actions";
 
 const ProductContext = createContext();
 const initialState = { isSidebarOpen: false };
@@ -7,10 +8,10 @@ const initialState = { isSidebarOpen: false };
 export function ProductProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
   const openSidebar = () => {
-    dispatch({ type: "SIDEBAR_OPEN" });
+    dispatch({ type: SIDEBAR_OPEN });
   };
   const closeSidebar = () => {
-    dispatch({ type: "SIDEBAR_CLOSE" });
+    dispatch({ type: SIDEBAR_CLOSE });
   };
   return (
     <ProductContext.Provider value={{ state, openSidebar, closeSidebar }}>
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -1,3 +1,6 @@
+import { SIDEBAR_OPEN, SIDEBAR_CLOSE } from "../actions";
+
+
 const reducer = (state, action) => {
   if (action.type === "SIDEBAR_OPEN") {
     return { ...state, isSidebarOpen: true };
